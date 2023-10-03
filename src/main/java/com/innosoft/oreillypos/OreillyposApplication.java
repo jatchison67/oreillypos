7b54c87f56d1075c857889adbabd806ef1f81531
John Atchison <johnatchison@Johns-MacBook-Pro.local>
2023-10-03 12:11:49 -0600

    Initial commit

---
+++ src/main/java/com/innosoft/oreillypos/OreillyposApplication.java
@@ -0,0 +1,13 @@
+package com.innosoft.oreillypos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OreillyposApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OreillyposApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/innosoft/oreillypos/OreillyposApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innosoft.oreillypos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OreillyposApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
